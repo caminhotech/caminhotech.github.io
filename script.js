0b1f2efdb69d5fe459e132c117843b57c4ce057f
Gilson <apostjp@gmail.com>
2024-11-22 14:01:25 -0300

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// Função de busca
 function search() {
     const input = document.getElementById('searchBar').value.trim();
-    const keyword = input.replace('#', ''); // Remove o # se for digitado.
+    const keyword = input.replace('#', '').toLowerCase(); // Remove o # se for digitado e converte para minúsculas.
 
-    // Redireciona para a página correspondente.
     if (keyword) {
         window.location.href = `produtos/${keyword}.html`;
     } else {
         alert('Por favor, digite um termo válido como #escova.');
     }
 }
+
+// Detectar Enter no campo de busca
+document.getElementById('searchBar').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        search();
+    }
+});
